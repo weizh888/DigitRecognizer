711f2433e102177a2287e20ce02cbb9825bc9a65
weizh888 <weizh888@gmail.com>
2017-01-17 18:32:13 -0500

    Ensemble submission files.

---
+++ ensemble_submissions.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import pandas as pd
+
+csv_path = './results'
+allFiles = [file for file in os.listdir(csv_path) if file.endswith('.csv')]
+#allFiles = ['aug_2_cycle.csv', 'submission_oldModel.csv', 'mnist-test5.csv', 'submission_0.4_relu.csv', 'submission_0.3_sigmoid.csv', 'mnist-test3.csv']
+alltest = pd.DataFrame()
+
+list_ = []
+for f in allFiles:
+    df = pd.read_csv(csv_path + '/' + f,index_col=None)
+    list_.append(df['Label'])
+alltest = pd.concat(list_, axis=1)
+#print (alltest)
+new_result = alltest.mode(axis=1)[0].astype(int) # select only the first mode value, and convert to integer label
+#print (new_result)
+d = {'ImageId': range(1,len(new_result)+1), 'Label':new_result} # create dataframe from dictionary
+new_submission = pd.DataFrame(d)
+new_submission.to_csv('./ensemble_result.csv', index=False, header=True)
